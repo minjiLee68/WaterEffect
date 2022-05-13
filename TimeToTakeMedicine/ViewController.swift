face70d4893dda788cd90c07c7b2ff7b4a8db4ea
minjiLee68 <dlalswl2598@gmail.com>
2022-05-13 16:57:55 +0900

    Initial Commit

---
+++ TimeToTakeMedicine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimeToTakeMedicine
+//
+//  Created by 이민지 on 2022/05/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
